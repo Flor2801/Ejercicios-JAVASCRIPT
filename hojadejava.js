bd81a83eac3980cbfb16b149cf46c83bc4f32e58
flor2801 <florenciasarria1@hotmail.com>
2020-08-18 22:32:25 -0300

    Ejercicios JS

---
+++ hojadejava.js
@@ -0,0 +1,51 @@
+let nombreusuario = prompt("Escriba su nombre")
+let apellidousuario = prompt("Escriba su apellido" )
+
+alert("Hola"+" "+nombreusuario+" "+apellidousuario)
+
+
+// SALUDO
+// Crear un programa que pida al usuario ingresar un nombre
+// y apellido por separado, y luego muestre un mensaje que diga, 
+// "Hola {nombre} {apellido}, bienvenida a Ada". 
+
+
+//let nombreusuario = prompt("Escriba su nombre")
+//let apellidousuario = prompt("Escriba su apellido")
+//let saludousuario = `Hola ${nombreusuario} ${apellidousuario}.`
+//alert(saludousuario)
+
+ 
+
+// HELADERIA
+// Crear un programa que pida al usuario ingresar, por separado, tres gustos de helado, y luego muestre
+// un mensaje que diga, por ejemplo, "Aquí tiene su helado de chocolate, dulce de leche y menta granizada".
+
+//alert("Ejercicio HELADERIA")
+//let gustoshelado = prompt("Ingrese primer gusto de helado") + " ," + prompt("Ingrese segundo gusto de helado") + " y" + prompt("Ingrese tercer gusto de helado") 
+//let eleccionhelados= `Aquí tiene su helado de ${gustoshelado}`
+//alert (eleccionhelados)
+
+
+
+// DATOS PERSONALES
+// Crear un programa que pida al usuario ingresar, por separado, nombre, apellido, edad, nacionalidad, documento, 
+// y muestre luego un mensaje que diga: "Nuevo usuario agregado al sistema:" y liste todos los datos juntos.
+
+//alert("Ejercicio DATOS PERSONALES")
+
+//let datospersonales = prompt("Ingrese su nombre de pila completo") + " ," + prompt("Ingrese su apellido")  + " ," + prompt("Ingrese su edad") + " ," +prompt("Ingrese su nacionalidad") + " ," + prompt("Ingrese su documento") 
+//let nuevousuario = `Nuevo usuario ingresado a sistema: ${datospersonales}.`
+//alert (nuevousuario)
+
+
+// LISTA DE REPRODUCCION
+// Crear un programa que pida al usuario ingresar, por separado, el nombre de una playlist y el título 
+// de tres canciones, y al finalizar se muestre un mensaje que 
+// diga, p. ej.: "Se ha creado la playlist 'Hits de los 80s' con las canciones 'Africa', 'Maniac', 
+//'Final Countdown'".
+
+
+//let playlist = prompt("Ingresa el nombre de tu playlist") 
+//let canciones = prompt("Ingresa la primera cancion") + " ," + prompt("Ingresa la segunda cancion") + " y" + prompt("Ingresa la tercera cancion") 
+//let playlistok = `Se ha creado la playlist ${playlist} con las canciones ${canciones}.`
